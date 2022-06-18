af471e198d29bd0d36d8aa47271bd987d7682f07
shubhamk099 <shubhamkkumar9797@gmail.com>
2022-06-18 15:38:29 +0530

    migration

---
+++ Migrations/20220618034023_firstmigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AttendanceProject.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
